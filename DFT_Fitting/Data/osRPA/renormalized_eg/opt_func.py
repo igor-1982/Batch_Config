38f335f8846fae1d19b7064de54b0b0c2d0d5ea9
Igor Ying Zhang <wenxinzy@gmail.com>
2018-07-02 13:51:46 +0800

    IYZ: test

--- DFT_Fitting/Data/osRPA/renormalized_eg/opt_func.py
+++ DFT_Fitting/Data/osRPA/renormalized_eg/opt_func.py
@@ -1,14 +1,18 @@
-def update_aims_scpt2(C,FitClass):
+def opt_func(C,P):
     '''\
-    update xc_functional group for SCPT2 type optimization in Aims package\
+    The function for optimization, where C is the parameters
     '''
-    FitClass.add_CMD=[]
-    FitClass.add_CMD.append('en_shift            %16i' %3)
-    FitClass.add_CMD.append('coupling_pt2_factor %16.8f' %C[0])
-    FitClass.add_CMD.append('coupling_pt2_screen %16.8f' %C[1])
-    FitClass.add_CMD.append('coupling_pt2_shift  %16.8f' %C[2])
-    FitClass.SCPT2_Para = C[3:]
-    return
+    a1, a3, a4, a5 = C
+    #a1 = 0.75
+    #a3 = 0.0
+    a2 = 1. - a1
+    #a5 = 0.0
+
+    E_noXC, Ex_HF, Ex_PBE, Ec_PBE, rsspt2, rospt2, Ex_SCAN, Ec_SCAN = P
+    #return E_noXC + a1*Ex_HF + a2*Ex_SCAN + a3*Ec_SCAN + a4*rospt2 + a5*rsspt2
+    return E_noXC + a1*Ex_HF + a2*Ex_PBE + a3*Ec_PBE + a4*rospt2 + a5*rsspt2
+    #return E_noXC + a1*Ex_HF + a2*Ex_PBE + a3*Ec_PBE + a4*rospt2 + \
+    #           a5*(Ex_SCAN-Ex_PBE) + a6*(Ec_SCAN-Ec_PBE)
 
 def opt_func_constrained(C,P):
     '''\
@@ -17,18 +21,6 @@ def opt_func_constrained(C,P):
     con = 0.5
     for a in C:
         if a<0 or a>1:
-	    con = -1
+            con = -1
             break
     return con
-
-def update_aims_scsrpa(C,FitClass):
-    '''\
-    update xc_functional group for SCPT2 type optimization in Aims package\
-    '''
-    FitClass.add_CMD=[]
-    print(C)
-    printString = 'renormalized_eg  '+len(C)*'%16.8f'
-    FitClass.add_CMD.append(printString %tuple(C))
-    printString = 'scsrpa_coeff  '+2*'%16.8f'
-    FitClass.add_CMD.append(printString %tuple([1.0,1.0]))
-    return
